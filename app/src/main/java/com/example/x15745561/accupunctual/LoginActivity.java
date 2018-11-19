fb86b45fd0fb94559e1796629f8b877b6beabc3c
Shaner125 <x15745561@student.ncirl.ie>
2018-11-19 21:34:22 +0000

    Changes to main activity, made splashscreen lauch activity.

--- app/src/main/java/com/example/x15745561/accupunctual/LoginActivity.java
+++ app/src/main/java/com/example/x15745561/accupunctual/LoginActivity.java
@@ -58,12 +58,12 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-//        btnReset.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
-//            }
-//        });
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, ResetPasswordActivity.class));
+            }
+        });
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
